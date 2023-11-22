bdb6cacc4044c92f50af9b043016c2c5a5443bb4
MalekMorad <malek-morad@hotmail.com>
2023-11-22 20:34:25 +0100

    testing placement

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn;
     private PlacementIndicator placementIndicator;
+    private ARPlaneManager planeManager;
 
     void Start ()
     {
         placementIndicator = FindObjectOfType<PlacementIndicator>();
+        planeManager = FindObjectOfType<ARPlaneManager>();
     }
 
     void Update ()
     {
-        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        if((Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) ||  Input.GetMouseButtonDown(0))
         {
             GameObject obj = Instantiate(objectToSpawn, 
             placementIndicator.transform.position, placementIndicator.transform.rotation);
--- Assets/Scripts/PlacementIndicator.cs
+++ Assets/Scripts/PlacementIndicator.cs
@@ -6,33 +6,57 @@ using UnityEngine.XR.ARSubsystems;
 
 public class PlacementIndicator : MonoBehaviour
 {
+    public GameObject car;
     private ARRaycastManager rayManager;
     private GameObject visual;
+    private bool carIsSet;
 
     void Start ()
     {
         // get the components
         rayManager = FindObjectOfType<ARRaycastManager>();
+        rayManager.gameObject.SetActive(false);
         visual = transform.GetChild(0).gameObject;
+        car.SetActive(false);
 
         // hide the placement visual
-        visual.SetActive(false);
+        carIsSet = false;
+        visual.SetActive(true);
+        
     }
 
     void Update ()
     {
-        // shoot a raycast from the center of the screen
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
 
-        // if we hit an AR plane, update the position and rotation
-        if(hits.Count > 0)
+        if (!carIsSet)
         {
-            transform.position = hits[0].pose.position;
-            transform.rotation = hits[0].pose.rotation;
+            // shoot a raycast from the center of the screen
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
 
-            if(!visual.activeInHierarchy)
-                visual.SetActive(true);
+            // if we hit an AR plane, update the position and rotation
+            if (hits.Count > 0)
+            {
+                transform.position = hits[0].pose.position;
+                transform.rotation = hits[0].pose.rotation;
+
+                if (!visual.activeInHierarchy)
+                {
+                    car.SetActive (true);
+                    visual.SetActive(true);
+
+                    if ((Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) || Input.GetMouseButtonDown(0))
+                    {
+                        GameObject obj = Instantiate(car,
+                        transform.position, transform.rotation);
+                        carIsSet = true;
+                    }
+                } else
+                {
+                    car.SetActive(false);
+                }
+
+            }
         }
     }
 }
