f333c504362ca7f7481dfb55570311ba88f2e48d
Lukas Jungwirth <lukas@brl-jungwirth.at>
2023-12-01 19:55:45 +0100

    add some basic textures and possiblity to change ball texture

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,25 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public Rigidbody ball;
-    private Vector3 initialPosition;
+    private Rigidbody ball;
+    private Renderer render;
+
     public float throwForce = 5;
+    public Material[] materials;
     void Start()
     {
         ball = GetComponent<Rigidbody>();
+        render = GetComponent<Renderer>();
+        render.sharedMaterial = materials[0];
+        
         ThrowBallUp();
     }
     void Update()
     {
+        //just for testing can be removed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int index = Array.IndexOf(materials, render.sharedMaterial);
+            if (index == 1)
+            {
+                ChangeColor(0);
+            }
+            else
+            {
+                ChangeColor(1);
+            }
+        }
+    }
 
+    void ChangeColor(int materialIndex)
+    {
+        render.sharedMaterial = materials[materialIndex];
     }
 
     void ThrowBallUp()
     {
-        ball.isKinematic = false;
+        Unfreeze();
         ball.AddForce(Vector3.up * throwForce, ForceMode.Impulse);
     }
 
@@ -36,7 +59,6 @@ public class Ball : MonoBehaviour
     
     void ToggleVisibility()
     {
-        Renderer render = GetComponent<Renderer>();
         if (render != null)
         {
             render.enabled = !render.enabled;
